6dc5243fac9fc861590acf4df97063380cf47ae8
Derek Smart <derek@grindaga.com>
2017-05-31 17:14:19 -0400

    basic getByArgs method
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/Model.php
+++ src/Model.php
@@ -23,4 +23,14 @@ abstract class Model extends PHPRed
         $query = "SELECT * FROM $this->table WHERE $this->primaryKey = $modelId;";
         return $this->query($query);
     }
+
+    public function getByArgs(array $args) : array
+    {
+        $search = '';
+        foreach ($args as $key => $value) {
+            $search .= '`' . $key . '` = "' . $value . '" ';
+        }
+        $query = "SELECT * FROM $this->table WHERE $search;";
+        return $this->query($query);
+    }
 }

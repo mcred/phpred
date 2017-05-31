57b0d6bf781efebf1b81577a95facca18aedd1a4
Derek Smart <derek@grindaga.com>
2017-05-31 16:43:05 -0400

    basic getById method
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/Model.php
+++ src/Model.php
@@ -5,15 +5,22 @@ abstract class Model extends PHPRed
 {
     public $model;
     public $table;
+    public $primaryKey;
 
     public function __construct(\mysqli $mysqli)
     {
         parent::__construct($mysqli);
     }
 
-    public function getAll()
+    public function getAll() : array
     {
         $query = "SELECT * FROM $this->table;";
+        return $this->query($query);
+    }
+
+    public function getById(int $modelId) : array
+    {
+        $query = "SELECT * FROM $this->table WHERE $this->primaryKey = $modelId;";
         return $this->query($query);
     }
 }

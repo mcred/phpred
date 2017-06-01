ef1f10082d92fee5f23f9cb79936e617a49732d2
Derek Smart <derek@grindaga.com>
2017-06-01 17:53:44 -0400

    basic query and sorting builder
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/PHPRed.php
+++ src/PHPRed.php
@@ -4,21 +4,53 @@ namespace PHPRed;
 abstract class PHPRed
 {
     protected $mysqli;
+    protected $conditions;
+    protected $order;
 
     public function __construct(\mysqli $mysqli)
     {
         $this->mysqli = $mysqli;
+        $this->conditions = '';
+        $this->order = '';
     }
 
     protected function query(string $query) : array
     {
         $results = $this->mysqli->query($query);
         if ($this->mysqli->errno) {
-            throw new \Exception($this->mysqli->error);
+            throw new \Exception($this->mysqli->error . $query);
         }
         if ($results->num_rows == 0) {
             throw new \Exception("No records found.");
         }
-        return $results->fetch_array(MYSQLI_ASSOC);
+        while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
+            $return[] = $row;
+        }
+        return $return;
+    }
+
+    public function orderBy(array $args)
+    {
+        $order = 'ORDER BY ';
+        foreach ($args as $key => $value) {
+            $key = $this->mysqli->escape_string($key);
+            $value = $this->mysqli->escape_string($value);
+            $order .= '`' . $key . '` ' . strtoupper($value) . ' ';
+        }
+        $this->order = trim($order);
+        return $this;
+    }
+
+
+    public function where(array $args, string $method = 'AND')
+    {
+        $search = 'WHERE ';
+        foreach ($args as $key => $value) {
+            $key = $this->mysqli->escape_string($key);
+            $value = $this->mysqli->escape_string($value);
+            $search .= '`' . $key . '` = "' . $value . '" ' . $method;
+        }
+        $this->conditions .= trim(substr($search, 0, -strlen($method)));
+        return $this;
     }
 }
